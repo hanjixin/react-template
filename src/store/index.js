325ff5aef82d96b888fceeff6ff63c31fe845a4b
hank <764671440@qq.com>
2019-11-01 17:57:08 +0800

    redux-thunk redux-logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,9 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware  } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
 import App from './reducers'
-let store = createStore(App)
+const loggerMiddleware = createLogger()
+let store = createStore(App, 
+  applyMiddleware(thunkMiddleware, // 允许我们 dispatch() 函数
+  loggerMiddleware ))// 一个很便捷的 middleware，用来打印 action 日志))
 export default store
